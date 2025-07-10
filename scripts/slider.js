cf72b9af66f1478a4ba2c0c05cd777d09d87993f
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-07-10 13:36:31 +0000

    start

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,7 +2,7 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 export function initSlider() {
-  const slider = new Swiper(".places-slider__body", {
+  const sliderPlaces = new Swiper(".places-slider__body", {
     navigation: {
       nextEl: ".places-slider__next",
       prevEl: ".places-slider__prev" 
@@ -29,7 +29,34 @@ export function initSlider() {
     breakpoints: { 
       1024: { 
         slidesPerView: 3,
-        // spaceBetween: 24, 
+        spaceBetween: 24, 
+      }
+    },
+  });
+  const sliderPlans = new Swiper(".plans-slider__body", {
+    autoHeight: true,
+    on: {
+      slideChangeTransitionEnd() {
+        this.updateAutoHeight();
+      }
+    },
+    speed: 800,
+    initialSlide: 1,
+    centeredSlides: true,
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+    spaceBetween: 16,
+    slidesPerView: 1.2,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true 
+    },
+    breakpoints: { 
+      1024: { 
+        slidesPerView: 3,
+        spaceBetween: 24, 
       }
     },
   });
