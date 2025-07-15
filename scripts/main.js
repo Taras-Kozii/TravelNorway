a5ce4665d0542be82170700c583d8d86aa59c89c
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-07-15 10:03:07 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@
 
 import { initSlider } from './slider.js';
 // import { initNavigation } from './menu.js';
-// import { initForms } from './forms.js';
 import { watcherAnim } from './effects/animation.js';
 import { countAnimate } from './counter.js';
+import { initForms } from './form.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initSlider();
   // initNavigation();
-  // initForms();
+  initForms();
   watcherAnim('[data-counter]', 0.2, countAnimate, false);
 });
