42eb025c27630146123fd87c83d0a543d485ea68
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-07-17 09:44:53 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 import { initSlider } from './slider.js';
 import { initNavigation } from './menu.js';
-import { watcherAnim } from './effects/animation.js';
+import { watcherAnim, watcherToggle } from './effects/animation.js';
 import { countAnimate } from './counter.js';
 import { initForms } from './form.js';
 import { inintHeader } from './header.js';
@@ -13,4 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   initForms();
   watcherAnim('[data-counter]', 0.2, countAnimate, false);
   inintHeader();
+  watcherToggle('.show-top', 0.2);
+  watcherToggle('.show-left-right', 0.2);
 });
